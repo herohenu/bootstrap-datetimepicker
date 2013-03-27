84b191d1d8b75aa45838591109faee23539a018f
Yinghao Lei <leiyinghao.bj@gmail.com>
2013-03-27 16:42:47 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Datepickerbs::Application.config.secret_token = 'a93980cdbfd390f372e36446dff129e4ddb01a3147728e9e0582dd5b55582b777844796a8be209173da9044903d34a2e9b21bc600950844c4db7ba6ce6f8a444'
